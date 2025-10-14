6642d8dacdf465201ea94bd8dfb82ffaa9f04e60
Juan <juanmetchart@gmail.com>
2025-10-13 23:25:54 -0300

    Add @UseGuards(JwtAuthGuard)

--- src/modules/trip/controllers/trip.controller.ts
+++ src/modules/trip/controllers/trip.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   ParseIntPipe,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { TripService } from '../services/trip.service';
 import { CreateTripDto } from '../dtos/create-trip.dto';
@@ -15,16 +16,19 @@ import { UpdateTripDto } from '../dtos/update-trip.dto';
 import { JoinTripDto } from '../dtos/join-trip.dto';
 import { TripResponse } from '../dtos/trip-response.dto';
 import { TripPassengerResponse } from '../dtos/trip-passenger-response.dto';
+import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
 
 @Controller('trips')
 export class TripController {
   constructor(private readonly tripService: TripService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createTripDto: CreateTripDto): Promise<TripResponse> {
     return this.tripService.create(createTripDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll(
     @Query('raceId', ParseIntPipe) raceId: number,
@@ -32,11 +36,13 @@ export class TripController {
     return this.tripService.findByRace(raceId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number): Promise<TripResponse> {
     return this.tripService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -45,16 +51,19 @@ export class TripController {
     return this.tripService.update(id, updateTripDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
     return this.tripService.remove(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('join')
   joinTrip(@Body() joinTripDto: JoinTripDto): Promise<TripPassengerResponse> {
     return this.tripService.joinTrip(joinTripDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':tripId/passengers/:passengerId')
   leaveTrip(
     @Param('tripId', ParseIntPipe) tripId: number,
@@ -63,6 +72,7 @@ export class TripController {
     return this.tripService.leaveTrip(tripId, passengerId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id/passengers')
   getPassengersByTrip(
     @Param('id', ParseIntPipe) id: number,
