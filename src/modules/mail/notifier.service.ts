1287e4ba36ca2234acce111134bbd3f89f713566
Juan <juanmetchart@gmail.com>
2025-11-11 17:20:42 -0300

    Remove Whatsapp temporaly

--- src/modules/mail/notifier.service.ts
+++ src/modules/mail/notifier.service.ts
@@ -11,27 +11,27 @@ export class NotifierService {
   ) {}
 
   async notifyTripCreated(driver: any, race: any, trip: any, tripUrl: string) {
-    await this.whatsappService.notifyTripCreated(driver, race, trip, tripUrl);
+    //await this.whatsappService.notifyTripCreated(driver, race, trip, tripUrl);
     await this.mailService.notifyTripCreated(driver, race, trip, tripUrl);
   }
 
   async notifyTripConfirmed(userProfile: any, race: any, trip: any, tripUrl: string) {
-    await this.whatsappService.notifyTripConfirmed(userProfile, race, trip, tripUrl);
+    //await this.whatsappService.notifyTripConfirmed(userProfile, race, trip, tripUrl);
     await this.mailService.notifyTripConfirmed(userProfile, race, trip, tripUrl);
   }
 
   async notifyTripJoin(driverProfile: any, race: any, passengerProfile: any, tripUrl: string) {
-    await this.whatsappService.notifyTripJoin(driverProfile, race, passengerProfile, tripUrl);
+    //await this.whatsappService.notifyTripJoin(driverProfile, race, passengerProfile, tripUrl);
     await this.mailService.notifyTripJoin(driverProfile, race, passengerProfile, tripUrl);
   }
 
   async notifyTripRejected(passengerProfile: any, race: any, trip: any, raceUrl: string) {
-    await this.whatsappService.notifyTripRejected(passengerProfile, race, trip, raceUrl);
+    //await this.whatsappService.notifyTripRejected(passengerProfile, race, trip, raceUrl);
     await this.mailService.notifyTripRejected(passengerProfile, race, trip, raceUrl);
   }
 
   async notifyTripLeaved(driverProfile: any, race: any, passengerProfile: any, tripUrl: string) {
-    await this.whatsappService.notifyTripLeaved(driverProfile, race, passengerProfile, tripUrl);
+    //await this.whatsappService.notifyTripLeaved(driverProfile, race, passengerProfile, tripUrl);
     await this.mailService.notifyTripLeaved(driverProfile, race, passengerProfile, tripUrl);
   }
 }
